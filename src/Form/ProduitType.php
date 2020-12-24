b728f26d45b6334949f0bcc4c11a77f829ecbd18
Charles <charles.anguenot@gmail.com>
2020-12-24 11:38:37 +0100

    #829 make:crud sur entité Produit

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('adresse')
+            ->add('codePostal')
+            ->add('ville')
+            ->add('prix')
+            ->add('objectif')
+            ->add('duree')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
