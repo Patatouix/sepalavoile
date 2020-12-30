84460f50a30a415a02b2bec70097c54c6ae0ad53
Charles <charles.anguenot@gmail.com>
2020-12-30 14:31:42 +0100

    #874 entité créneau d'event

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Creneau;
 use App\Entity\Produit;
 use App\Entity\ProduitType;
+use App\Repository\ProduitTypeRepository;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -16,36 +18,80 @@ class AppFixtures extends Fixture
         //produitTypes
         $produitEvent = new ProduitType();
         $produitEvent->setNom(ProduitType::PRODUIT_TYPE_EVENT_NAME);
+        $produitEvent->setSlug(ProduitType::PRODUIT_TYPE_EVENT_SLUG);
         $manager->persist($produitEvent);
 
         $produitDon = new ProduitType();
         $produitDon->setNom(ProduitType::PRODUIT_TYPE_DONATION_NAME);
+        $produitDon->setSlug(ProduitType::PRODUIT_TYPE_DONATION_SLUG);
         $manager->persist($produitDon);
 
         $produitAdhesion = new ProduitType();
         $produitAdhesion->setNom(ProduitType::PRODUIT_TYPE_ADHESION_NAME);
+        $produitAdhesion->setSlug(ProduitType::PRODUIT_TYPE_ADHESION_SLUG);
         $manager->persist($produitAdhesion);
 
-        //produits
+        //produits de type event
 
-        //events
-        for ($i = 1; $i <= 5; $i++) {
-            //même event, mais avec des dates différentes
-            for ($j = 1; $j <= 5; $j++) {
-                $produit = new Produit();
-                $produit->setNom('Event ' . $i);
-                $produit->setDescription('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam doloremque, dolores voluptates obcaecati nihil ipsam voluptatibus vero exercitationem in, debitis sapiente. Alias ullam culpa sint vel esse, numquam in?');
-                $produit->setDateDebut(new DateTime('2021-01-' . $j . ' 10:' . $j . ':19'));
-                $produit->setDateFin(new DateTime('2021-01-' . $j . ' 17:' . $j . ':25'));
-                $produit->setAdresse($i . ' rue du Verboté');
-                $produit->setCodePostal('90350');
-                $produit->setVille('Belfort');
-                $produit->setPrix($i);
-                $produit->setObjectif($i + 5);
-                $produit->setCreatedAt(new DateTime('NOW'));
-                $produit->setProduitType($produitEvent);
-                $manager->persist($produit);
+        for ($i = 1; $i <= 20; $i++) {
+            $produit = new Produit();
+            $produit->setNom('Event ' . $i);
+            $produit->setDescription('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam doloremque, dolores voluptates obcaecati nihil ipsam voluptatibus vero exercitationem in, debitis sapiente. Alias ullam culpa sint vel esse, numquam in?');
+            $produit->setDebutPublication(new DateTime('2021-01-' . $i . ' 08:' . $i . ':30'));
+            $produit->setFinPublication(new DateTime('2021-02-' . $i . ' 18:' . $i . ':50'));
+            $produit->setAdresse($i . ' rue du Verboté');
+            $produit->setCodePostal('90350');
+            $produit->setVille('Belfort');
+            $produit->setPrix($i);
+            $produit->setObjectif($i + 5);
+            $produit->setCreatedAt(new DateTime('NOW'));
+            $produit->setProduitType($produitEvent);
+            $manager->persist($produit);
+
+            //4 creneaux par Event
+            for ($j = 1; $j <= 4; $j++) {
+                $creneau = new Creneau();
+                $creneau->setProduit($produit);
+                $creneau->setDebut(new DateTime('2021-01-' . $j . ' 08:' . $j . ':30'));
+                $creneau->setFin(new DateTime('2021-01-' . $j . ' 18:' . $j . ':30'));
+                $manager->persist($creneau);
             }
+        }
+
+        //produits de type don
+
+        for ($i = 1; $i <= 5; $i++) {
+            $produit = new Produit();
+            $produit->setNom('Event ' . $i);
+            $produit->setDescription('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam doloremque, dolores voluptates obcaecati nihil ipsam voluptatibus vero exercitationem in, debitis sapiente. Alias ullam culpa sint vel esse, numquam in?');
+            $produit->setDebutPublication(new DateTime('2021-01-' . $i . ' 08:' . $i . ':30'));
+            $produit->setFinPublication(new DateTime('2021-02-' . $i . ' 18:' . $i . ':50'));
+            $produit->setAdresse($i . ' rue du Verboté');
+            $produit->setCodePostal('90350');
+            $produit->setVille('Belfort');
+            $produit->setPrix($i);
+            $produit->setObjectif($i + 5);
+            $produit->setCreatedAt(new DateTime('NOW'));
+            $produit->setProduitType($produitDon);
+            $manager->persist($produit);
+        }
+
+        //produits de type adhesion
+
+        for ($i = 1; $i <= 5; $i++) {
+            $produit = new Produit();
+            $produit->setNom('Event ' . $i);
+            $produit->setDescription('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam doloremque, dolores voluptates obcaecati nihil ipsam voluptatibus vero exercitationem in, debitis sapiente. Alias ullam culpa sint vel esse, numquam in?');
+            $produit->setDebutPublication(new DateTime('2021-01-' . $i . ' 08:' . $i . ':30'));
+            $produit->setFinPublication(new DateTime('2021-02-' . $i . ' 18:' . $i . ':50'));
+            $produit->setAdresse($i . ' rue du Verboté');
+            $produit->setCodePostal('90350');
+            $produit->setVille('Belfort');
+            $produit->setPrix($i);
+            $produit->setObjectif($i + 5);
+            $produit->setCreatedAt(new DateTime('NOW'));
+            $produit->setProduitType($produitAdhesion);
+            $manager->persist($produit);
         }
 
         $manager->flush();
---
+++ src/Entity/Creneau.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CreneauRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CreneauRepository::class)
+ */
+class Creneau
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $debut;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fin;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Produit::class, inversedBy="creneaux")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $produit;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): self
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): self
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getProduit(): ?Produit
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(?Produit $produit): self
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+}
