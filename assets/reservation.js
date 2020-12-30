4c40b79e70a64b394bbc05873461b8f4f877284b
Charles <charles.anguenot@gmail.com>
2020-12-30 14:38:05 +0100

    #845 calendrier vue réservation

--- assets/reservation.js
+++ assets/reservation.js
@@ -13,8 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendar = new Calendar(calendarEl, {
         plugins: [ dayGridPlugin ],
         initialView: 'dayGridMonth',
-        //tableau des events qui s'afficheront dans le calendrier
-        events: produits
+        events: creneaux,  //la variable creneaux a été construite dans le fichier de template de reservation
+        eventDidMount: function(info) {
+          //dans cette fonction on peut modifier le rendu du creneau
+        },
+        eventTimeFormat: {
+          //ici on modifie comment s'affiche l'heure des créneaux
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          meridiem: false
+        }
     });
 
   calendar.render();
