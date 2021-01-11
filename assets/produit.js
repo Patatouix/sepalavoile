e95cd644810aeb4158b6c2709a6de4c175169e67
Charles <charles.anguenot@gmail.com>
2021-01-11 13:46:28 +0100

    #908 #910 #918 #907 #913 #917 réoganisation des vues de donation et adhésion

--- assets/produit.js
+++ assets/produit.js
@@ -1,11 +1,28 @@
 import './styles/produit.css';
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY2hhcmxlc2FuZyIsImEiOiJja2psd2FlZHcwY3dtMnpxcHZqMndoNzcxIn0.QLuISkLVhz-4Ev3KHeCvSw'
-}).addTo(mymap);
+if (typeof L !== 'undefined') {
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiY2hhcmxlc2FuZyIsImEiOiJja2psd2FlZHcwY3dtMnpxcHZqMndoNzcxIn0.QLuISkLVhz-4Ev3KHeCvSw'
+    }).addTo(mymap);
+}
+
+$('input[type=range]').on("change", function() {
+    let value = $(this).val();
+    let text = value + ' €';
+    if (value > 75) {
+        text += ' <i class="fas fa-heart"></i>';
+    }
+    if (value > 50) {
+        text += ' <i class="fas fa-heart"></i>';
+    }
+    if (value > 25) {
+        text += ' <i class="fas fa-heart"></i>';
+    }
+    $('.rangeInputValue').html(text);
+});
