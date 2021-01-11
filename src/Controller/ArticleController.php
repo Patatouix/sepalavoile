b23879a6e9ab56ca289fedbf1015b03c9a4a895c
Charles <charles.anguenot@gmail.com>
2021-01-11 16:41:37 +0100

    Fixbug entité ArticleCOntroller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -24,7 +24,6 @@ class ArticleController extends AbstractController
     {
         return $this->render('article/index.html.twig', [
             'articles' => $articleRepository->findAll(),
-            
         ]);
     }
 
