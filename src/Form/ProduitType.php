017227ef5d9cb555428283a3580d55f239a8ecaa
Charles <charles.anguenot@gmail.com>
2021-02-02 08:24:11 +0100

    #1032 #1033 #1034 Système d'ajout de médias pour Produits & Articles

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Media;
 use App\Entity\Produit;
 use App\Entity\ProduitType as ProduitTypeEntity;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Form\Type\MediaEntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,6 +30,13 @@ class ProduitType extends AbstractType
                 'label' => 'Type de produit :',
                 'choice_label' => 'nom',
                 'class' => ProduitTypeEntity::class
+            ])
+            ->add('medias', MediaEntityType::class, [
+                'label' => 'Médias associés :',
+                'class' => Media::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+                'by_reference' => false,
             ])
             //->add('createdAt')
             //->add('updatedAt')
